85ca746c2656151a4c87a7ee71486f98efcaf3f3
Kushary <kusharysourav@gmail.com>
2021-09-16 13:02:01 +0530

    THA

---
+++ Day 5/require_test.js
@@ -0,0 +1,15 @@
+class Test {
+    print(){
+        var txt = "Have a nice day"
+        return txt
+    }
+}
+
+class Test2 {
+    print(){
+        var txt="Live your life"
+        return txt
+    }
+}
+
+module.exports={Test,Test2};
